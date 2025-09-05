ad948cf6595e7badad6b44e0d3c5e5dba0984a5e
Timclaz1 <lilpoopy2016@gmail.com>
2025-09-05 14:25:35 +0100

    sigin

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Header from "./components/Header";
 import SignUp from "./pages/SignUp";
 import Footer from "./components/Footer";
-import SignIn from "./pages/Login";
+import SignIn from "./pages/Signin";
 
 import { useState } from "react";
 
